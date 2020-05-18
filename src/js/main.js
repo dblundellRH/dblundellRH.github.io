75ce01373619f78e969a7301ac8ae8d4deba1e54
David Blundell <44975749+dblundellRH@users.noreply.github.com>
2020-05-18 09:47:25 -0400

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,13 @@ import frameworks from './definitions/frameworks.js';
 
 
 const OUTPUT_ELEMENT = 'stringOutput';
+const COOKIE_NAME = 'phdCount';
+const COUNT_ELEMENT = 'countOutput';
 
 const output = document.getElementById(OUTPUT_ELEMENT);
+const countOuput = document.getElementById(COUNT_ELEMENT);
 
 function getRandomNumberBetweenIndexRange(start = 0, end) {
-    console.log(Math.floor(Math.random() * end) + start);
     return Math.floor(Math.random() * end) + start;
 }
 
@@ -24,4 +26,43 @@ const subject = getRandomArrayItem(subjects);
 const location = getRandomArrayItem(locations);
 const framework = getRandomArrayItem(frameworks);
 
+function readCookies() {
+    const allCookies = document.cookie.split(';');
+
+    return allCookies.map(cookie => {
+        const temp = cookie.split('=');
+
+        return {
+            name: temp[0],
+            value: temp[1],
+        }
+    });
+}
+
+const cookies = readCookies();
+let count = 1;
+
+function writeCookie(value) {
+    console.log('write cookie', value);
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + 1);
+
+    document.cookie = `${COOKIE_NAME}=${value};max-age=31536000;secure;expires=${date.toUTCString()}`
+}
+
+if (cookies.some((cookie, index) => cookie.name === COOKIE_NAME)) {
+    const countCookie = cookies.filter(cookie => cookie.name === COOKIE_NAME);
+
+    console.log(countCookie[0].value);
+    count = parseInt(countCookie[0].value) + 1;
+
+    writeCookie(count);
+}
+else {
+    console.log('no cookie set');
+    writeCookie(count);
+}
+
 output.innerText = `${type} ${timePeriod} ${subject} ${location} ${framework}`;
+
+countOuput.innerText = count > 1 ? `You have created ${count} PhD topics` : '';
